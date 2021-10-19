2fe15c9fdedbcd2bfff602e7243faca35b63a943
Bryce Minor <bryce.minor7@gmail.com>
2021-10-19 14:18:36 -0500

    The removing a task feature has been completed.

--- task.js
+++ task.js
@@ -22,6 +22,12 @@ let showTasks = ``;
 // For storing the value of a new task
 let newTask;
 
+// For storing the number of the task to remove
+let num;
+
+// For storing the value of the removed task
+let removed;
+
 // Displays the menu for the user to select an option. Also, sets the user's response to the userInput variable
 let userInput = prompt(menu);
 
@@ -37,9 +43,10 @@ while (userInput !== `CLOSE`) {
             // Using \n to create a new line (AKA Line Break) after each task
             showTasks += `${tasks}\n`;
         }
-
+        
         // Display the current tasks to the user
         alert(showTasks);
+        
 
         // Set the value of the showTasks string variable back to an empty string
         showTasks = "";
@@ -56,6 +63,32 @@ while (userInput !== `CLOSE`) {
 
         // Adds the user's entry as a new item at the end of the tasks array
         tasks.push(newTask);
+    }
+
+    // Checks if the user entered REMOVE
+    if (userInput === `REMOVE`){
+
+        // Concatenates each task/item in the tasks array to the showTasks string variable. Also, sets/displays a number for each task.
+        for(i = 0; i < tasks.length; i++){
+
+            // Adding 1 to i (AKA The index) so the number will start displaying at 1. Also, using \n to create a new line (AKA Line Break)
+            showTasks += `${i + 1}: ${tasks[i]}\n`;
+        }
+
+        // Prompts the user to enter a number and stores their response to the num variable.
+        // Also, using \n to create a new line (AKA line break)
+        // Subtracting 1 from the user's entry so that it matches the index of the item to be removed from the tasks array.
+        // Just like with ParseInt, JavaScript will attempt to convert a string into a number when you try to Subtract from it. So ParseInt is not necessary here.
+        num = prompt(`Please enter a number to remove:\n${showTasks}`) -1;
+
+        // Removes the task/item selected by the user from the tasks array. Also, sets the task/item that was removed to the REMOVED variable
+        removed = tasks.splice(num, 1);
+
+        // Alerts user with the task/item that has been removed (NOTE: Using index on the REMOVED variable here because splice returns the value that is removed as an item in an array)
+        alert(`"${removed[0]}" has been removed`);
+
+        // Sets the value of the showTasks string variable back to an empoty string
+        showTasks = ``;
     }
 
     // Displays the menu again
