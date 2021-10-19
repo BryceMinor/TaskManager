1a29d16921bcaaad0cfbee87ebef190e97ac8129
Bryce Minor <bryce.minor7@gmail.com>
2021-10-19 15:00:35 -0500

    Added error handling feature to ensure the user enters a valid task to remove. Initial version of project complete.

--- task.js
+++ task.js
@@ -87,6 +87,17 @@ while (userInput !== `CLOSE`) {
         // Just like with ParseInt, JavaScript will attempt to convert a string into a number when you try to Subtract from it. So ParseInt is not necessary here.
         num = prompt(`Please enter a number to remove:\n${showTasks}`) -1;
 
+        // Loop that continues looping until the user enters a valid number
+        // Verifies the user entered a whole numer (AKA Not a decimal) that is one of the options in the prompt (AKA Within the number range of 1 to the length of the tasks array)
+        while(Math.floor(num) !== num || num < 0 || num >= tasks.length || !num){
+
+            // Alerts the user that they have NOT entered a valid number
+            alert(`Not a valid entry`);
+
+            // Prompts the user again as well as attempts to subtract 1 from their response
+            num = prompt(`Please enter a number to remove:\n${showTasks}`) -1;
+        }
+
         // Removes the task/item selected by the user from the tasks array. Also, sets the task/item that was removed to the REMOVED variable
         removed = tasks.splice(num, 1);
 
